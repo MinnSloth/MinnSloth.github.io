a0a2c8e357303eb01bda1cb64e9bc1df763029a5
MinnSloth <166810401+MinnSloth@users.noreply.github.com>
2024-04-12 22:30:44 +0700

    f

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "For Lyn Đần <3",
+    introTitle: 'Lyn Đần <3',
+    introDesc: `Iu mún chết đi được
+    mà sao cứ dỗi teo thế hiuhiu TvT`,
+    btnIntro: 'Kệ teo (Định nói câu này chứ gì)',
+    title: 'Hôm nay phải đi ngủ sớm nha lyn đừn <3',
+    desc: 'Ngủ trước 23h nhé hihi, (Không ngủ sớm là phải hết dỗi t) ',
+    btnYes: 'Okii An Đẹp Trai <3',
+    btnNo: 'Không ấy',
+    question: 'Oke nhớ ngủ sớm đó nhà, không ngủ sớm là phải hết dỗi teo bít chưaaa',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Bít òi (Định nhất "không" chứ gì hử đồ đần)',
+    mess: 'Bít thế là ngoan đấy nhé lyn đừn hihi',
+    messDesc: 'Ngủ ngon nhé !! Iu lyn đần.',
+    btnAccept: 'OK',
+    messLink: 'https://fb.com/thean2511'
+}
